b8e782c63909c8e55766f588a358fa74eb4021c2
orishim <shimonyori@gmail.com>
2023-12-19 07:55:23 +0000

    Update searchbar.tsx

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -44,7 +44,7 @@ export default function SearchBar() {
                 ref={searchRef}
                 name="search"
                 type="text"
-                placeholder="Describe your challenge, get solutions."
+                placeholder="Search by mechanism, project, or keyword"
                 className="pr-[62px] w-full shadow-lg hover:shadow-medium"
                 autoComplete="off"
               />
